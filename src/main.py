d485c7a5e4eaf040c7d3cd51f05dd6bb69104905
leonardogargani <leonardo.gargani@mail.polimi.it>
2021-12-28 12:04:46 +0100

    Add dynamic menu to open CSV files

--- src/main.py
+++ src/main.py
@@ -4,9 +4,12 @@ This file starts the program and shows one plot at a time.
 The byte to be shown can be selected through a dropdown menu.
 """
 
+import os
+import sys
+
 from PyQt5 import QtWidgets, uic
 import numpy as np
-import sys
+
 
 UI_FILE = 'ui/mainwindow.ui'
 INPUT_FOLDER = "../sample_data/"
@@ -17,8 +20,18 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         uic.loadUi(UI_FILE, self)
-        # start by plotting the first byte
-        self.plot_byte(1)
+        self.populate_menubar()
+        # plot the first byte by default
+        self.plot_byte(0)
+
+    def populate_menubar(self):
+        filenames = [f for f in os.listdir(INPUT_FOLDER) if f.endswith('.csv')]
+        for filename in sorted(filenames):
+            action = QtWidgets.QAction(filename, self)
+            byte_num = int(filename.partition('.')[0][-2:])
+            # due to python's scoping rules and closures, we need to capture byte_num
+            action.triggered.connect(lambda state, x=byte_num: self.plot_byte(x))
+            self.menuSelect.addAction(action)
 
     def plot_byte(self, byte_number):
         filename = 'data_byte_' + str(byte_number).zfill(2) + '.csv'
@@ -31,7 +44,7 @@ class MainWindow(QtWidgets.QMainWindow):
         for num in range(correlation_values.shape[1]):
             self.graph_widget.plot(time_instants, correlation_values[:, num])
 
-        self.top_label.setText("Byte #" + str(byte_number))
+        self.top_label.setText("Byte #" + str(byte_number).zfill(2))
 
 
 def main():
