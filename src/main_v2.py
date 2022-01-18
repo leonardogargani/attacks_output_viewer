3165e4bad31122a7faf0fc5bb41a0ea29e97f7b8
leonardogargani <leonardo.gargani@mail.polimi.it>
2022-01-18 20:20:10 +0100

    Improve rendering of buttons and delete an old demo

--- src/main_v2.py
+++ src/main_v2.py
@@ -1,4 +1,11 @@
+"""
+
+
+"""
+
+import os
 import sys
+
 from PyQt5 import QtWidgets, uic
 
 
@@ -9,7 +16,6 @@ NPY_DIRECTORY = "../sample_data/npy/"
 class GraphWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Window 2")
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -21,19 +27,31 @@ class MainWindow(QtWidgets.QMainWindow):
         self.create_buttons()
 
     def create_buttons(self):
+        filenames = [f for f in os.listdir(NPY_DIRECTORY) if f.endswith('.npy')]
+        row_index = 0
+        column_index = 0
+        for filename in sorted(filenames):
+            byte_num = int(os.path.splitext(filename)[0])
 
-        for i in range(4):
-            for j in range(4):
-                button = QtWidgets.QPushButton("test")
-                button.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-                self.gridLayout.addWidget(button, i, j)
+            button = QtWidgets.QPushButton('Byte ' + str(byte_num))
+            button.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+            button.setToolTip('click to show the plot')
+            # due to python's scoping rules and closures, we need to capture byte_num
+            button.clicked.connect(lambda state, x=byte_num: self.plot_byte(x))
 
-                button.setToolTip("click to show the plot")
-                button.clicked.connect(self.spawn_window)
+            self.gridLayout.addWidget(button, row_index, column_index)
 
-    def spawn_window(self):
+            # place the buttons in rows of 4 elements
+            if (column_index % 3 == 0) and (column_index != 0):
+                row_index += 1
+                column_index = 0
+            else:
+                column_index += 1
+
+    def plot_byte(self, byte_number):
         self.graph_window = GraphWindow()
         self.graph_window.show()
+        self.graph_window.setWindowTitle('Byte ' + str(byte_number))
 
 
 def main():
