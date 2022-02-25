333b323f0265ca7810013cb5f3a67a74b9917d90
leonardogargani <leonardo.gargani@mail.polimi.it>
2022-02-25 02:13:11 +0100

    Improve scripts readability

--- src/utils/csv_to_npy_converter.py
+++ src/utils/csv_to_npy_converter.py
@@ -9,15 +9,15 @@ CSV_DIRECTORY = "../../data/input/csv/"
 NPY_DIRECTORY = "../../data/output/npy/"
 
 
-# if not already done, convert real csv files to npy format
-for i in range(16):
-    filename = str(i)
-    if not os.path.exists(NPY_DIRECTORY + filename.zfill(2) + '.npy'):
+# If not already done, convert real csv files to npy format
+for byte_number in range(16):
+    filename = str(byte_number)
+    if not os.path.exists(f'{NPY_DIRECTORY}{filename.zfill(2)}.npy'):
         os.makedirs(NPY_DIRECTORY, exist_ok=True)
-        print('Loading ' + filename + '.csv...')
-        # "nan" values are replaced by 0
-        correlation_values = np.genfromtxt(CSV_DIRECTORY + filename + '.csv', delimiter=' ', filling_values=0)
-        print('Converting ' + filename + '.csv into npy...')
-        # save to npy format
-        np.save(NPY_DIRECTORY + str(i).zfill(2), correlation_values)
+        print(f'Loading {filename}.csv...')
+        # "NaN" values are replaced by 0
+        correlation_values = np.genfromtxt(f'{CSV_DIRECTORY}{filename}.csv', delimiter=',', filling_values=0)
+        print(f'Converting {filename}.csv into npy...')
+        # Save to npy format
+        np.save(f'{NPY_DIRECTORY}{filename.zfill(2)}', correlation_values)
 
--- src/utils/peak_detection.py
+++ src/utils/peak_detection.py
@@ -1,65 +1,69 @@
+"""
+Script that plots each byte and detects its peak saving the result in a CSV file.
+"""
+
 import numpy as np
 from matplotlib import pyplot as plt
-import os
 import matplotlib
+import os
 import gc
 
-matplotlib.use('agg')
 
 NPY_DIRECTORY = "../../data/output/npy/"
 PNG_DIRECTORY = "../../data/output/png/"
 CSV_DIRECTORY = "../../data/output/csv/"
 
-xMax = np.empty((16,),    dtype=int)
-yMax = np.empty((16,),    dtype=np.float64)
+
+matplotlib.use('agg')
+
+xMax = np.empty((16,), dtype=int)
+yMax = np.empty((16,), dtype=np.float64)
 lineMax = np.empty((16,), dtype=int)
 
 for byte_number in range(16):
-    print('-------------------- Byte #' + str(byte_number) + ' --------------------')
+    print(f'-------------------- Byte #{byte_number} --------------------')
     print('Loading file...')
-    correlation_values = np.load(NPY_DIRECTORY + str(byte_number).zfill(2) + '.npy', mmap_mode='r+')
+    correlation_values = np.load(f'{NPY_DIRECTORY}{str(byte_number).zfill(2)}.npy', mmap_mode='r+')
 
     # Check if the array contains only NaN values
     if np.all(np.isnan(correlation_values)):
         print('[WARNING] File contains only NaN values, peak detection aborted')
 
         # Save results
-        xMax[byte_number]    = -1
-        yMax[byte_number]    = -1
+        xMax[byte_number] = -1
+        yMax[byte_number] = -1
         lineMax[byte_number] = -1
 
     else:
-        # Replacing NaN values by 0
-        where_are_NaNs = np.isnan(correlation_values)
-        correlation_values[where_are_NaNs] = 0
+        # Replace NaN values by 0
+        correlation_values[np.isnan(correlation_values)] = 0
 
         peak = np.nanmax(correlation_values)
         index, line_number = np.where(correlation_values == peak)
 
-        print('Peak value    =', peak)  # Ymax
-        print('Sample number =', index[0])  # Xmax
-        print('Byte value    =', format(line_number[0], 'b').zfill(8))  # lineMax
+        print(f'Peak value    = {peak}')  # Ymax
+        print(f'Sample number = {index[0]}')  # Xmax
+        print(f'Byte value    = {format(line_number[0], "b").zfill(8)}')  # lineMax
 
-        if not os.path.exists(PNG_DIRECTORY + str(byte_number).zfill(2) + '_peak.png'):
-            os.makedirs(PNG_DIRECTORY, exist_ok=True)
+        if not os.path.exists(f'{PNG_DIRECTORY}{str(byte_number).zfill(2)}_peak.png'):
+            os.makedirs(f'{PNG_DIRECTORY}', exist_ok=True)
 
             print('Saving plot...')
 
             fig = plt.figure(figsize=(12, 4))
             plt.plot(correlation_values)
-            # plt.plot(correlation_values[:, line_number], color='blue', linewidth=0.01)
             plt.ylim([-1, 1.3])
             ax = plt.gca()
             xmax = index[0]
             ymax = peak
-            text = "Line {}, x={}, y={:.3f}".format(line_number[0], xmax, ymax)
+            text = f'Line {line_number[0]}, x={xmax}, y={ymax:.3f}'
             bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
             arrowprops = dict(arrowstyle="->", connectionstyle="angle,angleA=0,angleB=60")
             kw = dict(xycoords='data', textcoords="axes fraction",
                       arrowprops=arrowprops, bbox=bbox_props, ha="right", va="top")
             ax.annotate(text, xy=(xmax, ymax), xytext=(0.94, 0.96), **kw)
-            plt.title('Byte #' + str(byte_number).zfill(2))
-            plt.savefig(str(PNG_DIRECTORY + str(byte_number).zfill(2)) + '_peak.png')
+            plt.title(f'Byte #{str(byte_number).zfill(2)}')
+            plt.savefig(f'{str(PNG_DIRECTORY + str(byte_number).zfill(2))}_peak.png')
             # Free memory
             plt.close(fig)
             del fig, ax
@@ -74,8 +78,8 @@ for byte_number in range(16):
 if not os.path.exists(CSV_DIRECTORY):
     os.makedirs(CSV_DIRECTORY, exist_ok=True)
 
-f = open(CSV_DIRECTORY + 'peak.csv', 'w+')
+f = open(f'{CSV_DIRECTORY}peak.csv', 'w+')
 for i in range(16):
-    s = str(lineMax[i]) + ',' + str(xMax[i]) + ',' + str(yMax[i]) + '\r\n'
+    s = f'{lineMax[i]},{str(xMax[i])},{str(yMax[i])}\r\n'
     f.write(s)
 f.close()
